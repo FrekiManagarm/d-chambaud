2df50298ecc29fd6174d0c9d5aa0d340c9ba3a94
FrekiManagarm <mathchambaud@icloud.com>
2026-06-26 15:48:50 +0200

    Add Google Tag Manager

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Cormorant_Garamond, Montserrat } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Navigation from "@/components/Navigation";
 import LoadingScreen from "@/components/LoadingScreen";
@@ -95,6 +96,14 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${cormorant.variable} ${montserrat.variable}`}>
       <body className="min-h-screen">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRDT2Z7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: jsonLdScript(businessJsonLd) }}
@@ -105,6 +114,13 @@ export default function RootLayout({
         <Navigation />
         <Providers>{children}</Providers>
       </body>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MRDT2Z7');`}
+      </Script>
     </html>
   );
 }
